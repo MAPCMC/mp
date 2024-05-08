9263fd75c598cf01f9597973ab4a5d5d34928826
MAPCMC <maartenadampeene@gmail.com>
2024-05-08 14:33:29 +0200

    Add test layout and fix header grid

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html lang="nl" suppressHydrationWarning>
       <body
         className={cn(
-          "h-full min-w-full min-h-full ",
+          "h-full min-w-full min-h-full",
           "bg-white text-neutral-950 grid-light",
           "dark:text-white dark:grid-dark dark:bg-neutral-950",
           sans.variable,
@@ -43,9 +43,42 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <header className="fixed w-full flex p-2 justify-between items-center md:px-24">
+          <nav className="fixed w-full flex p-2 justify-between items-center md:px-24">
             <HeaderNav />
             <ThemeToggle />
+          </nav>
+          <header className="w-full flex flex-col items-center *:w-full *:md:max-w-[70%] space-y-2 pt-24 px-6 mb-2">
+            {/* logo puzzle */}
+            <div className="grid sm:grid-cols-[auto_1fr] grid-rows-[1fr_auto_auto] gap-y-1 !gap-x-4">
+              <div className="flex flex-wrap sm:inline-grid sm:grid-cols-[auto_auto_1fr] sm:grid-rows-[auto_auto_1fr] gap-1 sm:row-span-2 md:row-span-3">
+                <div className="size-12 lg:size-16 bg-red-400"></div>
+                <div className="size-12 lg:size-16 bg-red-400"></div>
+                <div className="size-12 lg:size-16 bg-red-400"></div>
+                <div className="size-12 lg:size-16 bg-red-400"></div>
+                <div className="size-12 lg:size-16 bg-red-400"></div>
+                <div className="size-12 lg:size-16 bg-red-400"></div>
+                <div className="size-12 lg:size-16 bg-red-400"></div>
+                <div className="size-12 lg:size-16 bg-red-400"></div>
+                <div className="size-12 lg:size-16 bg-red-400"></div>
+              </div>
+              {/* page header & intro */}
+
+              <h1 className="font-serif text-4xl font-bold sm:col-start-2 self-end">
+                Maarten Peene
+              </h1>
+              <p
+                role="doc-subtitle"
+                className="font-serif text-2xl sm:col-start-2"
+              >
+                <span>Creatieve</span> full-stack webdeveloper
+              </p>
+              <p className="sm:col-span-2 md:col-start-2 md:col-span-1">
+                Ik geloof in een internet waar creativiteit en functionaliteit
+                samenkomen. Als webontwikkelaar specialiseer ik me in het tot
+                leven brengen van experimentele ideeën, waarbij ik je begeleid
+                van concept naar boeiende online ervaringen.
+              </p>
+            </div>
           </header>
           {children}
         </ThemeProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
 import type { Metadata } from "next";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export const metadata: Metadata = {
   title: "Home | Maarten Peene",
@@ -7,28 +14,46 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="w-full flex flex-col items-center">
-      <div className="grid md:grid-cols-12 auto-rows-auto gap-x-4 py-24 px-8 md:max-w-[70%]">
-        {/* logo puzzle */}
-        <div className="md:row-span-4 md:col-span-4 md:row-start-1 bg-orange-500 w-full min-h-20 aspect-square max-w-[300px]"></div>
-
-        {/* page header & intro */}
-        <h1 className="md:col-span-8 md:row-start-2 items-end font-serif text-4xl font-bold">
-          Maarten Peene
-        </h1>
-        <p
-          role="doc-subtitle"
-          className="md:col-span-8 md:row-start-3 font-serif text-2xl"
-        >
-          <span>Creatieve</span> full-stack webdeveloper
-        </p>
-        <p className="md:col-span-8 md:row-start-4">
-          Ik geloof in een internet waar creativiteit en functionaliteit
-          samenkomen. Als webontwikkelaar specialiseer ik me in het tot leven
-          brengen van experimentele ideeën, waarbij ik je begeleid van concept
-          naar boeiende online ervaringen.
-        </p>
-      </div>
+    <main className="w-full flex flex-col items-center px-6 *:w-full *:md:max-w-[70%] *:gap-x-2 space-y-2">
+      <section className="grid grid-rows-2 grid-cols-4 gap-y-2">
+        <article>
+          <h2 className="font-serif text-3xl font-bold">Aanbod</h2>
+          <p>Wat kan ik voor je betekenen?</p>
+        </article>
+        <Card className="col-span-2">
+          <CardHeader>
+            <CardTitle>Websites & applicaties</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Concept & advies</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Freelance</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </section>
     </main>
   );
 }
