29673a7d8fbdfae33c2306c91c7e7df286e19687
MAPCMC <maartenadampeene@gmail.com>
2024-07-08 16:49:24 +0200

    Intro privacy statement

---
+++ app/privacyverklaring/page.tsx
@@ -0,0 +1,214 @@
+import { PageHeader } from "@/components/page-header";
+import { PageMain } from "@/components/page-main";
+
+export default function Privacy() {
+  return (
+    <>
+      <PageHeader>
+        <h1 className="font-serif text-4xl first-letter:capitalize">
+          Privacyverklaring
+        </h1>
+        <p>Laatst gewijzigd: 8 juli 2024</p>
+      </PageHeader>
+      <PageMain>
+        <article className="prose prose-sm prose-slate -ml-3 mt-6 max-w-[90ch] px-3 py-4 text-inherit md:prose-base prose-headings:font-serif prose-headings:font-normal prose-headings:text-inherit prose-h2:text-3xl prose-h3:text-2xl prose-li:marker:text-orange-500 prose-lead:text-lg prose-lead:text-inherit light:bg-light/50 basic:bg-basic/50 dark:bg-dark/50">
+          <p>
+            In deze verklaring zet ik uiteen hoe ik omga met persoongegevens die
+            aan mij worden toevertrouwd. Ik hecht veel waarde aan online
+            privacy, en doe er binnen mijn mogelijkheden alles om jouw gegevens
+            te beschermen. Lees deze verklaring door voor meer details.
+          </p>
+          <h2>Mijn gegevens</h2>
+          <dl>
+            <dt>bedrijfsnaam</dt>
+            <dd>Itoko</dd>
+            <dt>vestigingsadres</dt>
+            <dd>
+              <address>
+                Linnaeusstraat 16
+                <br /> 3553 CD Utrecht
+              </address>
+            </dd>
+            <dt>KvK-nummer</dt>
+            <dd>93973225</dd>
+          </dl>
+          <p>
+            Zit je met een vraag of wil je een verzoek indienen? Stuur een
+            e-mail naar info@maartenpeene.nl.
+          </p>
+          <h2>Jouw gegevens</h2>
+          Ik verwerk (mogelijk) de volgende persoonsgegevens:
+          <ul>
+            <li>voor- en achternaam;</li>
+            <li>e-mailadres;</li>
+            <li>telefoonnummer (vast en/of mobiel);</li>
+            <li>handelsnaam;</li>
+            <li>vestigingsadres en postadres;</li>
+            <li>KvK-nummer;</li>
+            <li>
+              klantnummer, opdrachtnummer, offertenummer en factuurnummer.
+            </li>
+          </ul>
+          <h3>Hoe kom ik aan jouw persoonsgegevens?</h3>
+          <p>
+            Ik heb deze persoonsgegevens van jou gekregen of ze komen uit
+            openbare registers, zoals het handelsregister van de KvK.
+          </p>
+          <h2>Redenen voor en bewaartermijnen van gegevens</h2>
+          <p>
+            Ik verwerk persoonsgegevens volgens de AVG en UAVG en alleen voor de
+            doeleinden die hieronder worden beschreven.
+          </p>
+          <h3>Contact</h3>
+          <p>
+            Heb je contact opgenomen per telefoon, e-mail, een contactformulier
+            op deze website, dan heb ik van jou gegevens ontvangen. Deze verwerk
+            ik om te kunnen reageren op jouw bericht. Ik bewaar deze gegevens
+            maximaal 5 jaar, zodat ik bijvoorbeeld kan bijhouden wanneer ons
+            laatste contact is geweest en om het gesprek inhoudelijk voort te
+            kunnen zetten. Het is mijn gerechtvaardigd belang om dit bij te
+            houden: ik wil niet vergeten wat we hebben besproken, zodat ik je
+            gericht kan benaderen.
+          </p>
+          <p>
+            Heb je contact opgenomen via social media, dan zal dat via een
+            persoonlijke account zijn geweest. Er is dan sprake van privé of
+            huiselijk gebruik.
+          </p>
+          <h3>Offertes</h3>
+          <p>
+            Vraag je een offerte op, dan verwerk ik je bedrijfs- en
+            persoonsgegevens om een offerte te kunnen sturen. Ik bewaar offertes
+            8 jaar, in verband met de wettelijke administratieplicht. Ik moet
+            aan kunnen tonen wat er aan een factuur vooraf is gegaan. Daarnaast
+            bewaar ik de offertes om bijvoorbeeld te kunnen zien wat de inhoud
+            van eerdere offerteaanvragen is geweest.
+          </p>
+          <h3>Facturatie</h3>
+          <p>
+            Ik verwerk de gegevens die noodzakelijk zijn voor de uitvoering van
+            een opdracht. Dat zijn in elk geval je naam en e-mailadres en
+            overige gegevens die noodzakelijk zijn om een correcte factuur te
+            kunnen sturen. Bij betaling zie ik van welke rekening dit komt en
+            wie de rekeninghouder is. Deze administratiegegevens (zoals
+            offertes, facturen en betaalgegevens) bewaar ik 8 jaar.
+          </p>
+          <h3>Uitvoering opdracht</h3>
+          <p>
+            Naast administratieve gegevens komt het voor dat er persoonsgebonden
+            gegevens worden uitgewisseld binnen een uit te voeren opdracht. Denk
+            hierbij aan gebruikersnamen of een IP-adres. Ik streef ernaar de
+            opslag van deze gegevens zoveel mogelijk in eigen beheer te laten,
+            en zal hierop aansturen in onze werkproces. Wanneer absoluut
+            noodzakelijk voor de uit te voeren opdracht verwerk ik deze gegevens
+            gedurende de overeenkomst.
+          </p>
+          <h3>Gerechtvaardigd belang</h3>
+          <p>
+            In sommige van bovenstaande gevallen heb ik een gerechtvaardigd
+            belang om gegevens te verwerken, bijvoorbeeld om mijn eigen
+            bewijspositie en rechtspositie te bewaken. Ik weeg dit belang altijd
+            af tegen het privacybelang van de persoon waarvan ik de
+            persoonsgegevens verwerk. Ik zal daarbij niet meer gegevens
+            verwerken dan nodig is om mijn doel te bereiken en mijn eigen
+            gerechtvaardigde belangen te waarborgen.
+          </p>
+          <h2>Overige ontvangers van persoonsgegevens</h2>
+          <p>Jouw gegevens worden (mogelijk) gedeeld met de volgende derden:</p>
+          <ul>
+            <li>hosting van de website;</li>
+            <li>hosting van cloudopslag;</li>
+            <li>mailservers;</li>
+            <li>telefoondiensten;</li>
+            <li>betaaldiensten;</li>
+            <li>bezorgdiensten.</li>
+          </ul>
+          <h3>Doorgifte derde landen</h3>
+          <p>
+            Ik verwerk persoonsgegevens alleen binnen de EU. Hosting van mijn
+            sites en servers gebeurt bij een Nederlandse partij, die werkt
+            binnen Nederlandse richtlijnen voor gegevensbescherming. Bij gebruik
+            van software of diensten die (mogelijk) gegevens buiten de EU
+            opslaan, ga ik het gebruik van persoonsgegevens uit de weg.
+          </p>
+          <h2>Jouw rechten</h2>
+          <p>
+            Je kan op verschillende manieren invloed uitoefenen op de verwerking
+            van jouw persoonsgegevens. Wil je een van deze rechten uitoefenen,
+            neem dan contact op per e-mail via info@maartenpeene.nl.
+          </p>
+          <h3>Recht op inzage, rectificatie en wissing</h3>
+          <p>
+            Je hebt het recht om te weten welke persoonsgegevens we van je
+            verwerken en je mag vervolgens verzoeken deze te laten rectificeren
+            of te wissen.
+          </p>
+          <h3>Beperking van de verwerking</h3>
+          <p>
+            Je hebt het recht ons te verzoeken de verwerking van jouw
+            persoonsgegevens te beperken, bijvoorbeeld, wanneer de gegevens niet
+            meer juist zijn, de verwerking onrechtmatig is, we jouw
+            persoonsgegevens niet meer nodig hebben of in afwachting van een
+            beslissing op het bezwaar dat je gemaakt hebt tegen de verwerking.
+          </p>
+          <h3>Recht op bezwaar</h3>
+          <p>
+            Je hebt het recht om bezwaar te maken tegen de verwerking van jouw
+            persoonsgegevens. Zolang ik niet wettelijk verplicht ben om jouw
+            gegevens te bewaren, zal ik aan dit verzoek voldoen.
+          </p>
+          <h3>Gegevensoverdraagbaarheid</h3>
+          <p>
+            Je hebt het recht jouw bewaarde gegevens in een gestructureerde,
+            gangbare en machinaal leesbare vorm te ontvangen, zodat je deze aan
+            een andere partij kunt overdragen, wanneer je ons toestemming had
+            gegeven om jouw gegevens te verwerken en wanneer de verwerking
+            geautomatiseerd is gedaan.
+          </p>
+          <h3>Toestemming intrekken</h3>
+          <p>
+            Heb ik je gegevens voor een bepaald doel verkregen op basis van jouw
+            toestemming, dan mag je deze toestemming altijd intrekken. Het
+            intrekken van de toestemming doet geen afbreuk aan de rechtmatigheid
+            van de verwerking vóór de intrekking.
+          </p>
+          <h3>Klachten</h3>
+          <p>
+            Heb je klachten over hoe er met jouw persoonsgegevens omgaan? Je
+            hebt het recht je klachten in te dienen bij de Autoriteit
+            Persoonsgegevens.
+          </p>
+          <h2>Beveiliging persoonsgegevens</h2>
+          <p>
+            Ik neem passende organisatorische en technische maatregelen om
+            persoonsgegevens te beveiligen tegen verlies of andere vormen van
+            onrechtmatige verwerkingen. Deze maatregelen zorgen voor een
+            beveiligingsniveau dat past bij de gegevens die ik verwerk.
+          </p>
+          <h2>Cookies</h2>
+          <p>Op dit moment maakt deze website geen gebruik van cookies.</p>
+          <p>
+            Wil je cookies uitschakelen of verwijderen? Dit kun je doen via je
+            browserinstellingen. Gebruik eventueel de help-functie van je
+            browser om te achterhalen hoe je dit kunt doen.
+          </p>
+          <h3>Links</h3>
+          <p>
+            Op deze site staan links naar externe websites. Deze externe
+            websites kunnen gebruik maken van cookies. Bekijk voor meer
+            informatie de cookie- of privacyverklaring van de betreffende
+            website.
+          </p>
+          <h2>Wijzigingen</h2>
+          <p>
+            Het kan zijn dat deze privacyverklaring wijzigt, bijvoorbeeld omdat
+            ik mijn diensten of werkwijze aanpas of deze website doorontwikkel.
+            Ik zal de nieuwe privacyverklaring op deze website publiceren. Als
+            de wijzigingen direct op jou van toepassing zijn, breng ik je op de
+            hoogte.
+          </p>
+        </article>
+      </PageMain>
+    </>
+  );
+}
