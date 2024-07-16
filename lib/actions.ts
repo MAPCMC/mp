a2cffcd0311f58f2d3a007a2e99f65cd0be90edd
MAPCMC <maartenadampeene@gmail.com>
2024-07-16 13:48:43 +0200

    add action handling with simple mail send

--- lib/actions.ts
+++ lib/actions.ts
@@ -8,20 +8,91 @@ import {
 } from "@/lib/validations";
 import { z } from "zod";
 // import { createQuotationRequest, createJobOffer } from "@/db/queries";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PW,
+  },
+});
+
+type SendEmailResponse = {
+  status: "SUCCESS" | "ERROR";
+  message: string;
+};
+
+const sendEmail = async ({
+  from,
+  to,
+  subject,
+  html,
+}: nodemailer.SendMailOptions): Promise<SendEmailResponse> => {
+  const mailOptions = {
+    from,
+    to,
+    subject,
+    html,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    if (info.response)
+      return { status: "SUCCESS", message: "Verzending gelukt" };
+  } catch (error) {
+    return { status: "ERROR", message: "Verzending mislukt" };
+  }
+};
+
+export default sendEmail;
 
 export async function sendContactRequest(data: z.infer<typeof contactSchema>) {
-  revalidatePath("/contact");
-  return data;
+  const mailResponse = await sendEmail({
+    from: process.env.EMAIL_ADDRESS,
+    to: process.env.EMAIL_ADDRESS,
+    subject: data.subject || "Contactformulier",
+    html: `
+      <p>Email: ${data.email}</p>
+      <p>Bericht: ${data.text}</p>
+    `,
+  });
+
+  return mailResponse;
 }
 
 export async function sendQuotationRequest(
   data: z.infer<typeof quotationSchema>,
 ) {
-  // createQuotationRequest(data);
-  return data;
+  const mailResponse = await sendEmail({
+    from: process.env.EMAIL_ADDRESS,
+    to: process.env.EMAIL_ADDRESS,
+    subject: "Offerte aanvraag",
+    html: `
+      <p>Naam: ${data.firstName} ${data.lastName}</p>
+      <p>Email: ${data.email}</p>
+      <p>Telefoon: ${data.phone}</p>
+      <p>Bericht: ${data.text}</p>
+    `,
+  });
+
+  return mailResponse;
 }
 
 export async function sendJobOffer(data: z.infer<typeof jobOfferSchema>) {
-  // createJobOffer(data);
-  return data;
+  const mailResponse = await sendEmail({
+    from: process.env.EMAIL_ADDRESS,
+    to: process.env.EMAIL_ADDRESS,
+    subject: "Beschikbaarheid aanvraag",
+    html: `
+      <p>URL: ${data.url}</p>
+      <p>Naam: ${data.firstName} ${data.lastName}</p>
+      <p>Email: ${data.email}</p>
+      <p>Telefoon: ${data.phone}</p>
+      <p>Bericht: ${data.text}</p>
+    `,
+  });
+
+  return mailResponse;
 }
