4d813e220930a164669ffbd50a412aa191c166dc
MAPCMC <maartenadampeene@gmail.com>
2024-11-30 22:17:47 +0100

    no more lenis

--- app/(site)/_layout/providers.tsx
+++ app/(site)/_layout/providers.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { ReactLenis } from "@/lib/lenis";
+
 import { ThemeProvider } from "@/app/(site)/_layout/theme-provider";
 
 export async function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-        themes={["basic", "light", "dark", "fun"]}
-      >
-        {children}
-      </ThemeProvider>
-    </ReactLenis>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      themes={["basic", "light", "dark", "fun"]}
+    >
+      {children}
+    </ThemeProvider>
   );
 }
